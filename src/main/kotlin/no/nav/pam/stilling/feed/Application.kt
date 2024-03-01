f347d6ffacd7850215afb8a03d1a4e9c844130e5
Tore Engvig <tore.engvig@nav.no>
2024-03-01 09:31:33 +0100

    Rydd grundigere

--- src/main/kotlin/no/nav/pam/stilling/feed/Application.kt
+++ src/main/kotlin/no/nav/pam/stilling/feed/Application.kt
@@ -70,8 +70,6 @@ fun startApp(
     val naisController = NaisController(healthService, prometheusRegistry)
     val kafkaListener = KafkaStillingListener(kafkaConsumer, feedService, healthService)
 
-    feedService.fjernDIRFraFeed()
-
     val javalin = startJavalin(
         port = 8080,
         jsonMapper = JavalinJackson(objectMapper),
